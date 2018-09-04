a4b7912f04758ba4f8cb061ca61b81f9769737b7
shoji_naoto <onyxnaoto@gmail.com>
2018-09-04 09:09:18 +0900

    0831
    comment.jsを編集、ajax実装
    コントローラ設定
    jbuilder作成
    ビュー編集

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,45 @@
+$(function(){
+  function buildHTML(comment){
+    var html = `<div class='media'>
+                  <div class='media-left'>
+                  <a href='/users/${comment.user_id}'>
+                  <img alt='64x64' class='media-object' data-holder='{&quot;rendered&quot;:&quot;true&quot;}' data-src=${comment.user_avatar} style='width: 64px; height: 64px;'>
+                  </a>
+                  </div>
+                  <div class='media-body'>
+                  <h4 class='top-aligned-media media-heading'>
+                  <strong>
+                  ${comment.user_name}
+                  <a rel="nofollow" data-method="delete" href="/prototypes/2/comments/2">削除</a>
+                  </strong>
+                  <p>
+                  ${comment.text}
+                  </p>
+                  </h4>
+                  </div>
+                  </div>`
+    return html;
+  }
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('#comment_list').append(html)
+      $('#textarea').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+})
