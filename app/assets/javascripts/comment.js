9c9fbbc4525225e7018b95131f969b4984e53a67
shoji_naoto <onyxnaoto@gmail.com>
2018-09-05 12:10:53 +0900

    0905
    コメントの非同期通信機能のエラー解消

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,48 +1,51 @@
-$(function(){
-  function buildHTML(comment){
-    var html = `<div class='media'>
-                  <div class='media-left'>
-                  <a href='/users/${comment.user_id}'>
-                  <img alt='64x64' class='media-object' data-holder='{&quot;rendered&quot;:&quot;true&quot;}' data-src=${comment.user_avatar} style='width: 64px; height: 64px;'>
-                  </a>
-                  </div>
-                  <div class='media-body'>
-                  <h4 class='top-aligned-media media-heading'>
-                  <strong>
-                  ${comment.user_name}
-                  <a rel="nofollow" data-method="delete" href="/prototypes/2/comments/2">削除</a>
-                  </strong>
-                  <p>
-                  ${comment.text}
-                  </p>
-                  </h4>
-                  </div>
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    function buildHTML(comment){
+      var html = `<div class='media'>
+                    <div class='media-left'>
+                      <a href='/users/${comment.user_id}'>
+                       <img alt='64x64' class='media-object' data-holder='{&quot;rendered&quot;:&quot;true&quot;}' data-src=${comment.user_avatar} style='width: 64px; height: 64px;'>
+                      </a>
+                    </div>
+                    <div class='media-body'>
+                      <h4 class='top-aligned-media media-heading'>
+                      <strong>
+                      ${comment.user_name}
+                     <a rel="nofollow" data-method="delete" href="/prototypes/2/comments/2">削除</a>
+                      </strong>
+                      <p>
+                    ${comment.text}
+                      </p>
+                      </h4>
+                    </div>
                   </div>`
-    return html;
-  }
-  $('#new_comment').on('submit', function(e){
-    e.preventDefault();
-    // console.log(this)
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      // console.log(data)
-      var html = buildHTML(data);
-      $('.row').append(html);
-      // $('.comment_field form-control').remove();
-      $('.comment_field form-control').prop('disabled', false);
-    })
-    .fail(function(){
-      alert('error');
-    })
-     // return false;
+      return html;
+    }
+    $('#new_comment').on('submit', function(e){
+      e.preventDefault();
+      // console.log(this)
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        // console.log(data)
+        var html = buildHTML(data);
+        $('#new_comment').append(html);
+        // $('.comment_field form-control').remove();
+        $('.comment_field.form-control').val('');
+        $('.comment_field.form-control').prop('disabled', false);
+      })
+      .fail(function(){
+        alert('error');
+      })
+       // return false;
+    });
   });
 });
