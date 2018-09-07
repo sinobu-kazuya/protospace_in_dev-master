423a3d351af20146b1329431f6d88d4a9ff30919
信夫知也 <kazooie.nobuo@gmail.com>
2018-09-07 10:27:46 +0900

    Edit preview image

---
+++ app/assets/javascripts/edit_image_review.js
@@ -0,0 +1,31 @@
+$(function() {
+  $('#prototype_captured_image_content').after('<span></span>');
+  // type=fileのinputの後にspanを入れる
+
+  // アップロードするファイルを選択
+  $('#prototype_captured_image_content').change(function(e) {
+    // type=fileのinputの入力内容が変更された時
+    var file = $(this).prop('files')[0];
+    //
+    // 画像以外は処理を停止
+    if (! file.type.match('image.*')) {
+      // クリア
+      $(this).val('');
+      $('span').html('');
+      return;
+    }
+
+    // 画像表示
+    var reader = new FileReader();
+    reader.onload = function() {
+    console.log("読み込みが終わりました");
+      var img_src = $('<img class=main_image>').attr('src', reader.result);
+      $(e.target).next().html(img_src);
+    }
+    reader.readAsDataURL(file);
+  });
+});
+
+
+
+
